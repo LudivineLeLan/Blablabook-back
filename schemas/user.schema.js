308684f6ba9c909369c4010e3450222e71431d4e
LudivineLeLan <lelanludivine@gmail.com>
2026-03-26 17:10:35 +0100

    updated email regex

--- schemas/user.schema.js
+++ schemas/user.schema.js
@@ -4,7 +4,7 @@ export const createUserSchema = Joi.object({
   name: Joi.string().trim().min(2).required(),
   firstname: Joi.string().trim().min(2).required(),
   age: Joi.number().integer().min(0).max(120),
-  email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'fr'] } }),
+  email: Joi.string().email({ minDomainSegments: 2 }).required(),
   password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")).required(),
   avatar: Joi.string().uri().optional(), 
 });
